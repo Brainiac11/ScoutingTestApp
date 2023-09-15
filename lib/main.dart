50f2d56e1c9af160a5f645311726964ac5211ba0
Saathvik Kamma <saathvikkamma@gmail.com>
2023-09-15 16:59:10 -0500

    optimized code

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -69,74 +70,74 @@ class _MyHomePageState extends State<MyHomePage> {
         foregroundColor: Colors.amber,
         backgroundColor: Colors.deepPurple,
       ),
-      body: Center(
+      body: const Center(
         child: Column(
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: ElevatedButton(
-                    onPressed: showSnackBar,
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      minimumSize: const Size(100, 100),
-                      maximumSize: const Size(100, 100),
-                      elevation: 10,
-                      shadowColor: Colors.black,
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 10, vertical: 10),
-                      textStyle: Theme.of(context).textTheme.headlineMedium,
-                    ),
-                    child: const Text("SnackBar"),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: ElevatedButton(
-                    onPressed: showSnackBar,
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      minimumSize: const Size(100, 100),
-                      maximumSize: const Size(100, 100),
-                      elevation: 10,
-                      shadowColor: Colors.black,
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 10, vertical: 10),
-                      textStyle: Theme.of(context).textTheme.headlineMedium,
-                    ),
-                    child: const Text("SnackBar"),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: ElevatedButton(
-                    onPressed: showSnackBar,
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      minimumSize: const Size(100, 100),
-                      maximumSize: const Size(100, 100),
-                      elevation: 10,
-                      shadowColor: Colors.black,
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 10, vertical: 10),
-                      textStyle: Theme.of(context).textTheme.headlineMedium,
-                    ),
-                    child: const Text("SnackBar"),
-                  ),
-                ),
-              ],
-            ),
+            ButtonRow(),
+            ButtonRow(),
+            ButtonRow(),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class ButtonRow extends StatelessWidget {
+  const ButtonRow({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        Padding(
+          padding: EdgeInsets.only(top: 30, bottom: 30),
+          child: Button(),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 30, bottom: 30),
+          child: Button(),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 30, bottom: 30),
+          child: Button(),
+        ),
+      ],
+    );
+  }
+}
+
+class Button extends StatelessWidget {
+  const Button({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: ElevatedButton(
+        onPressed: () {
+          if (kDebugMode) {
+            print("Hello from button");
+          }
+        },
+        style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(18),
+          ),
+          minimumSize: const Size(100, 100),
+          maximumSize: const Size(100, 100),
+          elevation: 10,
+          shadowColor: Colors.black,
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          textStyle: Theme.of(context).textTheme.headlineMedium,
+        ),
+        child: const Text("SnackBar"),
       ),
     );
   }
